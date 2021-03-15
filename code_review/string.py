f3bd436f907283a02423d0b7dac65fcf57346db4
Yan Zhou <38987117+yanzhoushow@users.noreply.github.com>
2021-03-15 03:20:17 -0400

    Update string.py

--- code_review/string.py
+++ code_review/string.py
@@ -1,5 +1,6 @@
 # common errors that should be caught during code review
-#  str.replace(old, new) is not in-place operation => new_str = str.replace(old, new)
+#  - str.replace(old, new) is not in-place operation => new_str = str.replace(old, new)
+#  - str delete a char or substring is through str.replace('pattern', '')
 
 string = 'xcdlzfdie'
 new_str = string.replace('i','c')
