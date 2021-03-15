6810301726596bbb7479273f06b4e74ef36e7537
Yan Zhou <38987117+yanzhoushow@users.noreply.github.com>
2021-03-15 03:19:02 -0400

    Create string.py

---
+++ code_review/string.py
@@ -0,0 +1,9 @@
+# common errors that should be caught during code review
+#  str.replace(old, new) is not in-place operation => new_str = str.replace(old, new)
+
+string = 'xcdlzfdie'
+new_str = string.replace('i','c')
+print(new_str)                                # 'xcdlzfdce'
+    
+upper_str = new_str.replace('c', 'C', 1)      # 1: only replace 1 occurance from left to right
+print(upper_str)                              # 'xCdlzfdce'
