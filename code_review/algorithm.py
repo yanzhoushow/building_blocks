c294ea7f870a4e267bff0c38fc3c8266fb033f00
Yan Zhou <38987117+yanzhoushow@users.noreply.github.com>
2021-03-14 23:12:43 -0400

    Update algorithm.py

--- code_review/algorithm.py
+++ code_review/algorithm.py
@@ -8,3 +8,12 @@ print(f'bisect result: {pos}')
 val = 4
 array.insert(pos, val)
 print(array)                 # [1,3,4,5,7]
+
+# bisect with range
+array = [9, 7, 1, 3, 5]
+start = 2
+end = 4
+value = 4
+pos = bisect.bisect(array, value, start, end)
+array.insert(pos, value)
+print(array)        # [9,7,1,3,4,5]
