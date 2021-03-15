6148924a5cafc61e18d274547a8de43031a3d3ff
Yan Zhou <38987117+yanzhoushow@users.noreply.github.com>
2021-03-14 21:18:01 -0400

    Create hash_dict.py

---
+++ code_review/hash_dict.py
@@ -0,0 +1,19 @@
+# covers code review takeaways for hash, dict, Counter, collections etc
+import collections
+
+# prefer defaultdict over {}
+memo = collections.defaultdict(list)
+memo_dict = {}
+
+print(memo['non_exist_key'])          # return [], preferred because it's shorter
+print(memo.get('non_exist_key'))      # return []
+print(memo_dict['non_exist_key'])     # KeyError exception!
+
+# Counter example
+ctr = collections.Count()
+string = 'abcddfefef'
+
+for c in string:
+  ctr[c]+=1
+  
+print(ctr['non_exist_char'])        # return 0
