c2fe6c4b4760a20cebc4592b387f695da5828d81
Yan Zhou <38987117+yanzhoushow@users.noreply.github.com>
2021-03-14 23:17:48 -0400

    bisect on string, bisect_left

--- code_review/algorithm.py
+++ code_review/algorithm.py
@@ -1,4 +1,7 @@
 # covers bisect, heapq
+# bisect works for both list and string
+# bisect(list/string, value, start, end) is same as bisect_right()
+# bisect_left(): insert before if value already exists in the list or string.
 import bisect
 
 array = [1, 3, 5, 7]
@@ -17,3 +20,11 @@ value = 4
 pos = bisect.bisect(array, value, start, end)
 array.insert(pos, value)
 print(array)        # [9,7,1,3,4,5]
+
+# bisect on string
+string = 'abcdefg'
+pos_right = bisect.bisect(string, 'b')
+print(pos_right)       # 2
+
+pos_left = bisect.bisect_left(string, 'b')
+print(pos_left)        # 1
